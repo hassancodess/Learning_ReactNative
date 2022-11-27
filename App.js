ff2d5dfdb263039f96c28eadf3a96dafa1bbe0b1
hassancodess <dev.hassancodes@gmail.com>
2022-11-27 14:20:23 +0500

    Initial Commit

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import Home from './screens/Home';
+import Register from './screens/Register';
+import Login from './screens/Login';
 
+const Stack = createStackNavigator();
 const App = () => {
   return (
-    <View>
-      <Text>Apps</Text>
-    </View>
-  )
-}
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Register" component={Register} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
-export default App
+export default App;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
