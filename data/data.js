13df71cbe6de146b157edd75b9d03c51cf8a775a
hassancodess <dev.hassancodes@gmail.com>
2022-12-01 10:45:51 +0500

    Store data in a seperate file

---
+++ data/data.js
@@ -0,0 +1,30 @@
+export const books = [
+  {
+    title: 'C#',
+    pages: 200,
+    author: 'Robert',
+    publisher: 'XYZ',
+    price: 1000,
+  },
+  {
+    title: 'Java',
+    pages: 200,
+    author: 'Robert',
+    publisher: 'XYZ',
+    price: 1000,
+  },
+  {
+    title: 'JavaScript',
+    pages: 200,
+    author: 'Robert',
+    publisher: 'XYZ',
+    price: 1000,
+  },
+  {
+    title: 'Python',
+    pages: 200,
+    author: 'Robert',
+    publisher: 'XYZ',
+    price: 1000,
+  },
+];
