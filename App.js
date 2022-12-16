0a4de27c60a085501c6b3ce75c6474407d0c2718
hassancodess <dev.hassancodes@gmail.com>
2022-12-16 12:22:53 +0500

    init

--- App.js
+++ App.js
@@ -1,211 +1,22 @@
-//npm install react-native-image-picker --save
-//Permission in Android
-//Project → android → app → src → debug → AndroidManifest.xml
-{
-  /* <uses-permission android:name="android.permission.CAMERA"/>
-<uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE"/> */
-}
-import React, {useState} from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  Image,
-  Platform,
-  PermissionsAndroid,
-} from 'react-native';
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import Add from './screens/Add';
+import List from './screens/List';
 
-import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
-
+const Stack = createStackNavigator();
 const App = () => {
-  const [filePath, setFilePath] = useState({});
-
-  const requestCameraPermission = async () => {
-    if (Platform.OS === 'android') {
-      try {
-        const granted = await PermissionsAndroid.request(
-          PermissionsAndroid.PERMISSIONS.CAMERA,
-          {
-            title: 'Camera Permission',
-            message: 'App needs camera permission',
-          },
-        );
-        // If CAMERA Permission is granted
-        return granted === PermissionsAndroid.RESULTS.GRANTED;
-      } catch (err) {
-        console.warn(err);
-        return false;
-      }
-    } else return true;
-  };
-
-  const requestExternalWritePermission = async () => {
-    if (Platform.OS === 'android') {
-      try {
-        const granted = await PermissionsAndroid.request(
-          PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
-          {
-            title: 'External Storage Write Permission',
-            message: 'App needs write permission',
-          },
-        );
-        // If WRITE_EXTERNAL_STORAGE Permission is granted
-        return granted === PermissionsAndroid.RESULTS.GRANTED;
-      } catch (err) {
-        console.warn(err);
-        alert('Write permission err', err);
-      }
-      return false;
-    } else {
-      return true;
-    }
-  };
-
-  const captureImage = async type => {
-    console.log('Capture Image ');
-    let options = {
-      mediaType: type,
-      maxWidth: 300,
-      maxHeight: 550,
-      quality: 1,
-      videoQuality: 'low',
-      durationLimit: 30, //Video max duration in seconds
-      saveToPhotos: true,
-      includeBase64: true,
-    };
-    let isCameraPermitted = await requestCameraPermission();
-    let isStoragePermitted = await requestExternalWritePermission();
-    if (isCameraPermitted && isStoragePermitted) {
-      console.log('Capture Image if block');
-      launchCamera(options, response => {
-        console.log('Response = ', response);
-
-        if (response.didCancel) {
-          alert('User cancelled camera picker');
-          return;
-        } else if (response.errorCode == 'camera_unavailable') {
-          alert('Camera not available on device');
-          return;
-        } else if (response.errorCode == 'permission') {
-          alert('Permission not satisfied');
-          return;
-        } else if (response.errorCode == 'others') {
-          alert(response.errorMessage);
-          return;
-        }
-        console.log('base64 -> ', response.assets[0].base64);
-        console.log('uri -> ', response.assets[0].uri);
-        console.log('width -> ', response.assets[0].width);
-        console.log('height -> ', response.height);
-        console.log('fileSize -> ', response.assets[0].fileSize);
-        console.log('type -> ', response.assets[0].type);
-        console.log('fileName -> ', response.assets[0].fileName);
-        setFilePath(response.assets[0]);
-      });
-    }
-  };
-
-  const chooseFile = type => {
-    let options = {
-      mediaType: type,
-      maxWidth: 300,
-      maxHeight: 550,
-      quality: 1,
-      includeBase64: true,
-    };
-    launchImageLibrary(options, response => {
-      console.log('Response = ', response);
-      console.log('Response Assets = ', response.assets[0].uri);
-
-      if (response.didCancel) {
-        alert('User cancelled camera picker');
-        return;
-      } else if (response.errorCode == 'camera_unavailable') {
-        alert('Camera not available on device');
-        return;
-      } else if (response.errorCode == 'permission') {
-        alert('Permission not satisfied');
-        return;
-      } else if (response.errorCode == 'others') {
-        alert(response.errorMessage);
-        return;
-      }
-      console.log('Response again.. = ', response);
-      console.log('base64 -> ', response.assets[0].base64);
-      console.log('uri -> ', response.assets[0].uri);
-      console.log('width -> ', response.assets[0].width);
-      console.log('height -> ', response.assets[0].height);
-      console.log('fileSize -> ', response.assets[0].fileSize);
-      console.log('type -> ', response.assets[0].type);
-      console.log('fileName -> ', response.assets[0].fileName);
-      setFilePath(response.assets[0]);
-    });
-  };
-
   return (
-    <View style={{flex: 1}}>
-      <Text style={styles.titleText}>Image Picker in React Native</Text>
-      <View style={styles.container}>
-        <Text>Base64 Image</Text>
-        {
-          <Image
-            source={{
-              uri: `data:${filePath.type};base64,${filePath.base64}`,
-            }}
-            style={styles.imageStyle}
-          />
-        }
-        <Text>Uri Image</Text>
-        <Image source={{uri: filePath.uri}} style={styles.imageStyle} />
-
-        <TouchableOpacity
-          activeOpacity={0.5}
-          style={styles.buttonStyle}
-          onPress={() => chooseFile('photo')}>
-          <Text style={styles.textStyle}>Choose Image</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          activeOpacity={0.5}
-          style={styles.buttonStyle}
-          onPress={() => captureImage('photo')}>
-          <Text style={styles.textStyle}>Capture Image</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Add">
+        <Stack.Screen name="Add" component={Add} />
+        <Stack.Screen name="List" component={List} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
 export default App;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 10,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-  },
-  titleText: {
-    fontSize: 22,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    paddingVertical: 20,
-  },
-  textStyle: {
-    padding: 10,
-    color: 'black',
-    textAlign: 'center',
-  },
-  buttonStyle: {
-    alignItems: 'center',
-    backgroundColor: '#DDDDDD',
-    padding: 5,
-    marginVertical: 10,
-    width: 250,
-  },
-  imageStyle: {
-    width: 200,
-    height: 200,
-    margin: 5,
-  },
-});
+const styles = StyleSheet.create({});
---
+++ screens/Add.js
@@ -0,0 +1,156 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Platform,
+  PermissionsAndroid,
+} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {TextInput, Button} from 'react-native-paper';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+import {openDatabase} from 'react-native-sqlite-storage';
+const db = openDatabase({name: 'ProductDatabase.db'});
+
+const Add = () => {
+  const [name, setName] = useState('');
+
+  const [filePath, setFilePath] = useState({});
+  useEffect(() => {
+    CREATE_TABLE();
+  }, []);
+  useEffect(() => {
+    console.log(users);
+  }, [users]);
+
+  const handleAddUser = () => {
+    ADD_USER();
+    setName('');
+    setFilePath({});
+    // console.log(name, filePath.uri);
+  };
+  const handleShowUser = () => {
+    FETCH_USERS();
+  };
+  const CREATE_TABLE = () => {
+    let query =
+      'CREATE TABLE IF NOT EXISTS Users(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, imageUri TEXT)';
+    db.transaction(txn => {
+      txn.executeSql(
+        query,
+        [],
+        (tx, res) => {
+          console.log('Users Table Created Successfully');
+        },
+        error => {
+          console.log('ERROR');
+        },
+      );
+    });
+  };
+  const ADD_USER = () => {
+    let query = 'INSERT INTO Users(name, imageUri) VALUES(?,?)';
+    db.transaction(txn => {
+      txn.executeSql(
+        query,
+        [name, filePath.uri],
+        (tx, res) => {
+          console.log('User Added');
+        },
+        error => {
+          console.log('ERROR');
+        },
+      );
+    });
+  };
+  const FETCH_USERS = () => {
+    let query = `SELECT * FROM Users`;
+    db.transaction(txn => {
+      txn.executeSql(
+        query,
+        [],
+        (tx, res) => {
+          let resultsSet = [];
+          for (let i = 0; i < res.rows.length; ++i) {
+            let record = res.rows.item(i);
+            resultsSet.push(record);
+          }
+          setUsers(resultsSet);
+        },
+        error => {
+          console.log('ERROR');
+        },
+      );
+    });
+  };
+
+  const chooseFile = type => {
+    let options = {
+      mediaType: type,
+      maxWidth: 300,
+      maxHeight: 550,
+      quality: 1,
+    };
+    launchImageLibrary(options, response => {
+      console.log('Response = ', response);
+      console.log('Response Assets = ', response.assets[0].uri);
+
+      if (response.didCancel) {
+        alert('User cancelled camera picker');
+        return;
+      } else if (response.errorCode == 'camera_unavailable') {
+        alert('Camera not available on device');
+        return;
+      } else if (response.errorCode == 'permission') {
+        alert('Permission not satisfied');
+        return;
+      } else if (response.errorCode == 'others') {
+        alert(response.errorMessage);
+        return;
+      }
+      console.log('Response again.. = ', response);
+      //   console.log('base64 -> ', response.assets[0].base64);
+      console.log('uri -> ', response.assets[0].uri);
+      console.log('width -> ', response.assets[0].width);
+      console.log('height -> ', response.assets[0].height);
+      console.log('fileSize -> ', response.assets[0].fileSize);
+      console.log('type -> ', response.assets[0].type);
+      console.log('fileName -> ', response.assets[0].fileName);
+      setFilePath(response.assets[0]);
+    });
+  };
+  return (
+    <View>
+      <View>
+        <TextInput
+          label="Enter name"
+          value={name}
+          onChangeText={newValue => setName(newValue)}
+        />
+      </View>
+      <Text>Uri Image</Text>
+      <Image source={{uri: filePath.uri}} style={styles.imageStyle} />
+      <Button mode="contained" onPress={() => chooseFile('photo')}>
+        Choose Photo
+      </Button>
+      <Button mode="contained" onPress={handleAddUser}>
+        Add User
+      </Button>
+      <Button mode="contained" onPress={handleShowUser}>
+        Show User
+      </Button>
+    </View>
+  );
+};
+
+export default Add;
+
+const styles = StyleSheet.create({
+  imageStyle: {
+    width: 200,
+    height: 200,
+    margin: 5,
+    borderWidth: 5,
+    borderColor: 'red',
+  },
+});
