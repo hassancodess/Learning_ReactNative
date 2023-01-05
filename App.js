4b8fa51054bb3206ec110e45877c771833613c04
hassancodess <dev.hassancodes@gmail.com>
2023-01-05 22:04:19 +0500

    Init

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
-import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+// Screens
 
+const Stack = createNativeStackNavigator();
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Notifications" component={Notifications} />
+        <Stack.Screen name="Profile" component={Profile} />
+        <Stack.Screen name="Settings" component={Settings} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
-
 export default App;
-
-const styles = StyleSheet.create({});
